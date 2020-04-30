20c18777c70bc9eda15dbecc263e586c58564ece
xu <20726131@qq.com>
2020-04-30 19:26:53 +0800

    添加拦截器

---
+++ src/main/java/com/example/jizm/service/TokenService.java
@@ -0,0 +1,16 @@
+package com.example.jizm.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.jizm.model.User;
+import org.springframework.stereotype.Service;
+
+@Service("TokenService")
+public class TokenService {
+    public String getToken(User user) {
+        String token="";
+        token= JWT.create().withAudience(user.getUserId().toString())// 将 user id 保存到 token 里面
+                .sign(Algorithm.HMAC256(user.getPassword()));// 以 password 作为 token 的密钥
+        return token;
+    }
+}
